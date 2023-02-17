679a38d6b3db56370e1c551a20ad7a1ae8b5b9c9
BlueFeet <b1uefeet.dev@gmail.com>
2023-02-17 18:16:48 -0500

    Proyecto Vacio

---
+++ src/main/java/com/example/demo/EjercicioU3PaP3ScApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioU3PaP3ScApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioU3PaP3ScApplication.class, args);
+	}
+
+}
